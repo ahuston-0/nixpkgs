87d90da77084451d6e388c12baa8eaa1e43b5511
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-06-19 17:11:48 +0200

    python312Packages.pytest-openfiles: remove
    Leaf package, that is unmaintained, deprecated and archived.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12287,8 +12287,6 @@ self: super: with self; {
 
   pytest-notebook = callPackage ../development/python-modules/pytest-notebook { };
 
-  pytest-openfiles = callPackage ../development/python-modules/pytest-openfiles { };
-
   pytest-order = callPackage ../development/python-modules/pytest-order { };
 
   pytest-param-files = callPackage ../development/python-modules/pytest-param-files { };
