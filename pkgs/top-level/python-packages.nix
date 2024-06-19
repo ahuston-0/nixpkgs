f6fc3ad89ff871493f322f46eb68b1cb36da1cc1
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-06-19 22:29:49 +0200

    python312Packages.pytest-lazy-fixtures: init at 1.0.7

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12265,6 +12265,8 @@ self: super: with self; {
 
   pytest-lazy-fixture = callPackage ../development/python-modules/pytest-lazy-fixture { };
 
+  pytest-lazy-fixtures = callPackage ../development/python-modules/pytest-lazy-fixtures { };
+
   pytest-localserver = callPackage ../development/python-modules/pytest-localserver { };
 
   pytest-logdog = callPackage ../development/python-modules/pytest-logdog { };
